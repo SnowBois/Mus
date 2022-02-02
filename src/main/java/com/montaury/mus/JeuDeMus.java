8c2985dfee5d8cf637b82df29cdad470fa3623a0
Aurélien Vantalon <avantalon@iutbayonne.univ-pau.fr>
2022-02-02 16:29:11 +0100

    Adaptation du main pour intégrer le système d'équipes

--- src/main/java/com/montaury/mus/JeuDeMus.java
+++ src/main/java/com/montaury/mus/JeuDeMus.java
@@ -1,20 +1,34 @@
 package com.montaury.mus;
 
-import com.montaury.mus.jeu.Partie;
 import com.montaury.mus.console.AffichageEvenements;
-import com.montaury.mus.jeu.joueur.Joueur;
 import com.montaury.mus.jeu.Opposants;
+import com.montaury.mus.jeu.Partie;
+import com.montaury.mus.jeu.joueur.Joueur;
+
 import java.util.Scanner;
 
 public class JeuDeMus {
   public static void main(String[] args) {
+    System.out.print("Entrez le nombre de joueur par equipe( 1 ou 2) :");
+    var nombreJoueurParEquipe = new Scanner(System.in).nextInt();
     System.out.print("Entrez votre nom: ");
     var nomJoueur = new Scanner(System.in).next();
     var joueurHumain = Joueur.humain(nomJoueur);
 
-    var partie = new Partie(new AffichageEvenements(joueurHumain));
-    var resultat = partie.jouer(new Opposants(joueurHumain, Joueur.ordinateur()));
 
+    Joueur tabEquipe1[nombreJoueurParEquipe] = {joueurHumain};
+    Joueur tabEquipe2[nombreJoueurParEquipe] = {Joueur.ordinateur()};
+
+    if (nombreJoueurParEquipe != 1) {
+      tabEquipe1[1] = Joueur.ordinateur();
+      tabEquipe2[1] = Joueur.ordinateur();
+    }
+
+    var equipe1 = new Equipe(tabEquipe1);
+    var equipe2= new Equipe(tabEquipe2);
+
+    var partie = new Partie(new AffichageEvenements(joueurHumain));
+    var resultat = partie.jouer(new Opposants(equipe1,equipe2));
     System.out.println("Le vainqueur de la partie est " + resultat.vainqueur().nom());
   }
 }
