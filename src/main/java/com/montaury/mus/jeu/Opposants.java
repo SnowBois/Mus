a4f7c2247cdb3c1af719142424fc213afddc281a
Simon Longy <slongy@iutbayonne.univ-pau.fr>
2022-02-15 01:01:16 +0100

    Amélioration de l'ordre de passage (passant de Deque à ArrayDeque), les modes 1v1 et 2v2 s'exécutent sans planter

--- src/main/java/com/montaury/mus/jeu/Opposants.java
+++ src/main/java/com/montaury/mus/jeu/Opposants.java
@@ -2,20 +2,21 @@ package com.montaury.mus.jeu;
 
 import com.montaury.mus.jeu.joueur.Joueur;
 import com.montaury.mus.jeu.joueur.Equipe;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Deque;
+
+import java.util.*;
 
 public class Opposants {
   private Equipe equipe1;
   private Equipe equipe2;
   private Joueur joueurEsku;
   private Joueur joueurZaku;
-  private Deque<Joueur> ordreDePassage;
+  private ArrayDeque<Joueur> ordreDePassage;
 
   public Opposants(Equipe equipe1, Equipe equipe2) {
     this.equipe1 = equipe1;
     this.equipe2 = equipe2;
+
+    this.ordreDePassage = new ArrayDeque<Joueur>();
 
     for (var i = 0 ; i < equipe1.listeJoueurs().length ; i++)
     {
@@ -44,6 +45,6 @@ public class Opposants {
   }
 
   public List<Joueur> dansLOrdre() {
-    return List.of(ordreDePassage);
+    return new ArrayList(ordreDePassage);
   }
 }
